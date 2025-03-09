f411f1c0760f7544059fedb890aee4af4414345a
Andrey <snick25rs@gmail.com>
2025-03-09 23:21:37 +0100

    поменял на правильный settings

--- church/church/settings.py
+++ church/church/settings.py
@@ -137,4 +137,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = os.getenv('IP_HOST')
+CSRF_TRUSTED_ORIGINS = [
+    os.getenv('IP_HOST')
+]
