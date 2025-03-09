a4ea6f5c4a5c8f2f975920295b5b9f58bb023be7
Andrey <snick25rs@gmail.com>
2025-03-09 23:26:51 +0100

    добавил allowed_hosts ip_host

--- church/church/settings.py
+++ church/church/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('PASSWORD_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("IP_HOST")]
 
 
 # Application definition
