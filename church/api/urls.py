601dc8c070844e31ac257cf8b2eef774d27e6435
Andrey <snick25rs@gmail.com>
2025-03-10 21:05:49 +0100

    Синхронизировал docs_url с DEBUG

--- church/api/urls.py
+++ church/api/urls.py
@@ -1,6 +1,12 @@
 # api/urls.py
 from django.urls import path
 from .views import api
+from ninja import NinjaAPI
+
+import os
+
+if os.getenv('DEBUG') is False:
+    api = NinjaAPI(docs_url=None)
 
 urlpatterns = [
     path("", api.urls),
