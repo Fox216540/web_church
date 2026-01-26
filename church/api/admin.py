a0b9121496dcd137687c9d7835b8d86a40fa0e23
Andrey <snick25rs@gmail.com>
2026-01-26 01:59:56 +0100

    Добавил регистрацию новых моделей в админ-панель.

--- church/api/admin.py
+++ church/api/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Sermon, Event
+from .models import (
+	Sermon,
+	Event,
+	Content,
+	CategoryOfContent,
+	HomeGroup,
+	ChurchDocument,
+	Ministry,
+	ChurchContact,
+	ChurchBoard
+)
 
 admin.site.register(Sermon)
 admin.site.register(Event)
-
+admin.site.register(Content)
+admin.site.register(CategoryOfContent)
+admin.site.register(HomeGroup)
+admin.site.register(ChurchDocument)
+admin.site.register(Ministry)
+admin.site.register(ChurchContact)
+admin.site.register(ChurchBoard)
