0c5898e5cded0aab7178a3df6a96c5cc4d0a6120
Andrey <snick25rs@gmail.com>
2026-01-26 02:00:17 +0100

    Добавил новые URL-маршруты для документации, домашних групп, совета церкви и контента.

--- church/main/urls.py
+++ church/main/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.main, name='home'),
     path('sermons/', views.sermons, name='sermons'),
-    path('confession_of_faith/', views.confession_of_faith, name='confession'),
+    path('docs/', views.docs, name='docs'),
+	path('doc/', views.doc, name='doc'),
+	path('home-group/', views.home_groups, name='home-group'),
+	path('board/', views.board, name='board'),
+	path('content/', views.content, name='content'),
 ]
